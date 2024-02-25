12a9c40dd628641601f1aeaabfc6e0a38663eb53
itsshreyashk <shreyashkumar.me@gmail.com>
2024-02-25 15:13:21 +0530

    configs

--- client/app/sign/auth/page.tsx
+++ client/app/sign/auth/page.tsx
@@ -1,11 +1,27 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-
+import Props from './props'
 const Page = () => {
+    const usernameRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        document.title = "Sign In";
+
+        if (usernameRef && usernameRef.current) {
+            usernameRef.current.addEventListener('keypress', (e) => {
+                if (e.key === 'Enter') {
+                    alert("Working...")
+                }
+            })
+        }
+        return () => {
+        }
+    }, [])
+
     return (
         <>
+            <Props usernameRef={usernameRef} />
         </>
     )
 }
---
+++ client/app/sign/auth/props.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+interface PropsProps {
+    usernameRef: React.RefObject<any>,
+}
+const Props: React.FC<PropsProps> = ({ usernameRef }) => {
+    return (
+        <>
+            <div className="flex justify-center py-[25vh]">
+                <div className="w-[80%] max-w-[350px] min-w-[max-content] border h-[max-content] rounded-xl px-4 py-2">
+                    <div className="w-full text-start space-y-2">
+                        <div className="space-y-0">
+                            <label htmlFor="username" className='text-sm'>Username</label>
+                            <input type="text" name="username" className="px-4 py-2 w-full border rounded-full outline-none text-sm" ref={usernameRef} />
+                        </div>
+                        <div className="space-y-0">
+                            <label htmlFor="password" className='text-sm'>Password</label>
+                            <input type="password" name="password" className="px-4 py-2 w-full border rounded-full outline-none text-sm" />
+                        </div>
+                    </div>
+                    <div className="pt-7 space-y-1">
+                        <button type="button" className='w-full text-center text-sm px-4 py-2 bg-blue-600 text-white rounded-full active:bg-blue-800'>Sign In</button>
+                        <button type="button" className='w-full text-center text-sm px-4 py-2 bg-gray-100 text-gray-800 rounded-full active:bg-gray-300'>Forgot Password</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Props;
