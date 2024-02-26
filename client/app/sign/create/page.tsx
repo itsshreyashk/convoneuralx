ae98730bd07c7aed6feb7b64f77f12a468df4652
itsshreyashk <shreyashkumar.me@gmail.com>
2024-02-26 16:11:34 +0530

    sign up option created

--- client/app/sign/create/page.tsx
+++ client/app/sign/create/page.tsx
@@ -2,12 +2,20 @@
 
 import React, { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/navigation'
-
+import Props from './props'
 const Page = () => {
-    const router = useRouter()
+    const router = useRouter();
+    const [passView, setPassView] = useState('password');
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
     const handleGoBack = () => {
         router.back();
     };
+    const handleSignUp = () => {
+
+    }
     useEffect(() => {
 
         document.title = "Sign Up";
@@ -18,6 +26,7 @@ const Page = () => {
     }, [])
     return (
         <>
+            <Props usernameRef={usernameRef} passwordRef={passwordRef} handleSignUp={handleSignUp} passView={passView} setPassView={setPassView} ageRef={ageRef} emailRef={emailRef} />
             <div className="absolute top-4 left-4">
                 <span className="text-blue-800 text-lg font-bold cursor-pointer hover:underline hover:text-blue-800 text-sm" onClick={handleGoBack}>{'Back'}</span>
             </div>
--- client/app/sign/create/props.tsx
+++ client/app/sign/create/props.tsx
@@ -1,11 +1,48 @@
-'use client'
 import React from 'react'
-
-const Props: React.FC = () => {
-
+interface PropsProps {
+    usernameRef: React.RefObject<any>,
+    passwordRef: React.RefObject<any>,
+    handleSignUp: () => void,
+    passView: any,
+    setPassView: any,
+    ageRef: React.RefObject<any>,
+    emailRef: React.RefObject<any>,
+}
+const Props: React.FC<PropsProps> = ({ usernameRef, passwordRef, handleSignUp, passView, setPassView, ageRef, emailRef }) => {
     return (
-        <></>
+        <>
+            <div className="flex justify-center py-[20vh]">
+                <div className="w-[80%] max-w-[350px] min-w-[max-content] border h-[max-content] rounded-xl px-4 py-2 backdrop-blur-xl">
+                    <div className="w-full text-start space-y-2">
+                        <div className="space-y-0">
+                            <label htmlFor="username" className='text-sm'>Create Username</label>
+                            <input type="text" name="username" className="px-4 py-2 w-full border rounded-full outline-none text-sm" ref={usernameRef} />
+                        </div>
+                        <div className="space-y-0">
+                            <label htmlFor="password" className='text-sm'>Create Password</label>
+                            <div className="flex">
+                                <input type={`${passView}`} name="password" className="px-4 py-2 w-full border border-r-0 rounded-l-full outline-none text-sm" ref={passwordRef} />
+                                <button type="button" className='text-sm px-4 py-2 rounded-r-full bg-white border border-l-0' onClick={() => {
+                                    (passView === 'password') ? `${setPassView('text')}` : `${setPassView('password')}`
+                                }}>{(passView === 'password') ? 'view' : 'hide'}</button>
+                            </div>
+                        </div>
+                        <div className="space-y-0">
+                            <label htmlFor="email" className='text-sm'>Email</label>
+                            <input type="email" name='email' className='px-4 py-2 w-full border rounded-full outline-none text-sm' ref={emailRef} />
+                        </div>
+                        <div className="space-y-0">
+                            <label htmlFor="age" className='text-sm'>Age</label>
+                            <input type="number" name='age' className='px-4 py-2 w-full border rounded-full outline-none text-sm' ref={ageRef} />
+                        </div>
+                    </div>
+                    <div className="pt-7 space-y-1">
+                        <button type="button" className='w-full text-center text-sm px-4 py-2 bg-blue-600 text-white rounded-full active:bg-blue-800' onClick={handleSignUp}>Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
-export default Props
+export default Props;
