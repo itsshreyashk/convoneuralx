30201a847dc797c27352caba42aa83e58f0f9df1
itsshreyashk <shreyashkumar.me@gmail.com>
2024-03-10 21:27:45 +0530

    ui improvements

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -20,11 +20,11 @@ const Page: React.FC = () => {
                 <div className="w-full px-4 py-2 flex justify-between items-center">
                     <div className="text-start">
                         <Link href="/">
-                            <h1 className="text-gray-800 font-bold py-2 disable_select">Convoneuralx</h1>
+                            <h1 className="text-gray-800 font-bold py-2 disable_select flex">Convoneuralx <span className="material-symbols-outlined">chevron_right</span><span className="material-symbols-outlined text-sm text-gray-600">home</span></h1>
                         </Link>
                     </div>
                     <div className="flex space-x-2">
-                        <span className='text-blue-600 text-sm py-3 font-bold hover:underline cursor-pointer'>{dislplayName}</span>
+                        <span className='text-blue-600 text-sm py-3 font-bold hover:underline cursor-pointer make_responsive_displayname'>{dislplayName}</span>
                         <img src={picURL} alt="profile" className='rounded-full w-11 bg-white disable_select' />
                         <div className="h-11 w-11 flex items-center justify-center">
                             <span className="material-symbols-outlined cursor-pointer hover:border-gray-600 border active:opacity-70 disable_select active:bg-gray-400">
@@ -36,15 +36,27 @@ const Page: React.FC = () => {
             </nav>
             <Dashboard />
             <div className="p-4">
-                <h1 className='font-bold'>Your Models</h1>
-                <div className="w-full text-center">
-                    <span className='text-sm text-gray-600'>No models to show currently</span>
+                <div className="border border-dotted border-gray-800 p-4 rounded-xl">
+                    <div className="flex">
+                        <h1 className='font-bold'>Your Models</h1>
+                        <button type="button" className='text-sm active:opacity-[0.7]'>
+                            <span className="material-symbols-outlined text-sm">
+                                refresh
+                            </span>
+                        </button>
+
+                    </div>
+                    <div className="w-full text-center">
+                        <span className='text-sm text-gray-600'>No models to show currently</span>
+                    </div>
                 </div>
+
             </div>
-            <footer className="p-4 flex justify-center">
+            <footer className="flex justify-center fixed bottom-0 w-full">
                 <div className="w-full space-y-2 p-4 bg-black">
                     <a className='text-blue-800 text-sm cursor-pointer active:text-blue-600 active:scale-[0.9] transform duration-200' href=''>Explore Docs</a><br />
-                    <a className='text-blue-800 text-sm cursor-pointer active:text-blue-600 active:scale-[0.9] transform duration-200' href=''>Explore Codebase</a>
+                    <a className='text-blue-800 text-sm cursor-pointer active:text-blue-600 active:scale-[0.9] transform duration-200' href=''>Explore Codebase</a><br />
+                    <a className='text-blue-800 text-sm cursor-pointer active:text-blue-600 active:scale-[0.9] transform duration-200' href=''>View Usage schemes</a>
                 </div>
             </footer>
         </>
